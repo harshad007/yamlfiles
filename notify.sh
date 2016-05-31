d650d2b3b91bbb71cfc65b8c2e730001068f13b0
harshad007 <hvelapure@yahoo.com>
2016-05-31 23:10:41 +0530

    Create notify.sh

---
+++ notify.sh
@@ -0,0 +1,5 @@
+curl -X POST --data-urlencode 
+'payload={"channel": "#build-info", "username": "webhookbot", 
+"text": "This is posted to #build-info and comes from a bot named webhookbot. With Build_ID: #'${BUILD_ID}'", 
+"icon_emoji": ":ghost:"}' 
+https://hooks.slack.com/services/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
