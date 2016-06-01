f141118d7cc497f84e51ad5129507e0be5d41667
harshad007 <hvelapure@yahoo.com>
2016-06-02 02:44:43 +0530

    Update notify.groovy

--- notify.groovy
+++ notify.groovy
@@ -1,2 +1,2 @@
 def payload='payload={"channel": "#build-info", "username": "webhookbot", "text": "This is posted to #build-info and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}'
-groovy -e "println 'curl -i -H \'Content-Type:application/json\' -XPOST -d \'${payload}\' https://hooks.slack.com/services/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/'.execute().text"
+"println 'curl -i -H \'Content-Type:application/json\' -XPOST -d \'{\"channel\":\"#build-info\"}\' https://hooks.slack.com/services/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/'.execute().text"
