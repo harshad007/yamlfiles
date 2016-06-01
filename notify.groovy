99c90bb3210ac6fd0b263a4235e83fa96fa31b70
harshad007 <hvelapure@yahoo.com>
2016-06-01 22:40:38 +0530

    Update notify.groovy

--- notify.groovy
+++ notify.groovy
@@ -1,12 +1,1 @@
-import groovy.json.JsonOutput
-
-def notifySlack(text, channel) {
-    def slackURL = 'https://hooks.slack.com/services/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'
-    def payload = JsonOutput.toJson([channel: "#build-info",
-                                      text:  text,
-                                     username: "jenkins",
-                                     icon_emoji: ":monkey:"])
-    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
-}
-notifySlack()
-    println "hello"
+ "sh curl -X POST --data-urlencode \'payload={channel: #build-info}\' https://hooks.slack.com/services/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"
