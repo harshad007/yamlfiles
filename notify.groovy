9b88b132c6ea276cec71683e5f259da41934f3a6
harshad007 <hvelapure@yahoo.com>
2016-06-09 19:52:33 +0530

    Update notify.groovy

--- notify.groovy
+++ notify.groovy
@@ -1,4 +1,14 @@
-def test3(){
-["curl", "-i", "-XPOST", "--data-urlencode", 'payload={"channel": "#build-info", "username": "webhookbot", "BUILD_ID", "${BUILD_ID}", "text": "This is groovy script generated notification", "icon_emoji": ":monkey:"}', 'https://hooks.slack.com/services/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'].execute().text
+def notifySlack() {
+    def slackURL = 'https://hooks.slack.com/services/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'.execute().text
+    def payload = JsonOutput.toJson([channel: channel,
+                                      text:  text,
+                                     username: "jenkins",
+                                     icon_emoji: ":jenkins:"])
+    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
 }
-test3()
+notifySlack()
+
+#def test3(){
+#["curl", "-i", "-XPOST", "--data-urlencode", 'payload={"channel": "#build-info", "username": "webhookbot", "BUILD_ID", "${BUILD_ID}", "text": "This is groovy script generated notification", "icon_emoji": ":monkey:"}', 'https://hooks.slack.com/services/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'].execute().text
+#}
+#test3()
