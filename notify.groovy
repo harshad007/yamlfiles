c77077a231bc3eb4a9d8105f392fb4fcdbe2bbef
harshad007 <hvelapure@yahoo.com>
2016-06-09 19:00:50 +0530

    Update notify.groovy

--- notify.groovy
+++ notify.groovy
@@ -1,4 +1,8 @@
 def test3(){
-["curl", "-i", "-XPOST", "--data-urlencode", 'payload={"channel": "#build-info", "username": "webhookbot", "text": "This is groovy script generated notification", "icon_emoji": ":monkey:"}', 'https://hooks.slack.com/services/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'].execute().text
+["curl", "-i", "-XPOST", "--data-urlencode", 'payload={"channel": "#build-info",
+"username": "webhookbot",
+"text": "This is groovy script generated notification",
+"icon_emoji": ":monkey:"}',
+'https://hooks.slack.com/services/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'].execute().text
 }
 test3()
