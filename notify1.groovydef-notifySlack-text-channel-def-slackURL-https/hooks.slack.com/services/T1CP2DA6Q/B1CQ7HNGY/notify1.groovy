15bd82d2a3e294d08d1a21efee2e18f4cd8d1cb3
harshad007 <hvelapure@yahoo.com>
2016-06-09 19:51:52 +0530

    Update notify1.groovy

--- notify1.groovydef-notifySlack-text-channel-def-slackURL-https/hooks.slack.com/services/T1CP2DA6Q/B1CQ7HNGY/notify1.groovy
+++ notify1.groovydef-notifySlack-text-channel-def-slackURL-https/hooks.slack.com/services/T1CP2DA6Q/B1CQ7HNGY/notify1.groovy
@@ -1,5 +1,5 @@
 def notifySlack() {
-    def slackURL = 'https://hooks.slack.com/services/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'
+    def slackURL = 'https://hooks.slack.com/services/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'.execute().text
     def payload = JsonOutput.toJson([channel: channel,
                                       text:  text,
                                      username: "jenkins",
