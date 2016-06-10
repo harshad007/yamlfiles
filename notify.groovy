33198e970058538dc818ce690bcbd97b8e2341a8
harshad007 <hvelapure@yahoo.com>
2016-06-10 17:28:26 +0530

    Update notify.groovy

--- notify.groovy
+++ notify.groovy
@@ -1,3 +1,4 @@
+def BUILD_NUMBER=env.BUILD_NUMBER
 def test3(){
 ["curl", "-i", "-XPOST", "--data-urlencode", 'payload={"channel": "#build-info", "username": "webhookbot", "text": "This is \'${BUILD_NUMBER}\' generated notification", "icon_emoji": ":monkey:"}', 'https://hooks.slack.com/services/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'].execute().text
 }
