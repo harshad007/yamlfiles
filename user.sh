99ff27e443da102363143dfb0531d462c6eafc07
harshad007 <hvelapure@yahoo.com>
2016-06-15 12:25:33 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,2 +1,1 @@
-
-curl -i -X POST--data-urlencode 'payload={"channel": "#build-info", "username": "webhookbot", "text": "This is groovy script generated notification \'${BUILD_NUMBER}\' '\t' \'${BUILD_STATUS}'\  ", "icon_emoji": ":monkey:"}' 'https://hooks.slack.com/services/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'
+curl -X POST --data-urlencode 'payload={"channel": "#build-info", "username": "webhookbot", "text": "This is posted to #build-info and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
